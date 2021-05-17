3602a626fec761ddb8a46cda14eb91000eb8f6ad
Hector Vurchio <hectorvurchio@gmail.com>
2021-05-17 09:09:03 -0400

    third commit

--- Election2021.java
+++ Election2021.java
@@ -3,10 +3,16 @@ import java.util.StringTokenizer;
 import java.util.ArrayList;
 import java.io.IOException;
 import java.util.ListIterator;
+/**
+* The Election2021 runs an application that will be used to give examples
+*  of some data structures and algorithms as well as talk about Java .
+*
+* @author  Hector Jose Vurchio Hurtado
+* @version 1.0
+* @since   2021-05-15 
+*/
 public class Election2021{
 	public static void main(String[] arg){
 		
 	}
 }
-
-
