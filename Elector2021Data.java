632b491af1832c5412eaf5baf68515d4b26d3704
Hector Vurchio <hectorvurchio@gmail.com>
2021-07-12 19:56:38 -0400

    Sort using Compatable Interface

---
+++ Elector2021Data.java
@@ -0,0 +1,62 @@
+package societyAperson;
+import java.time.LocalDate;
+import java.time.LocalTime;
+/**
+* Elector2021Data is a class that works with Election2021 class 
+* in an application that will be used to give examples of some data structures
+* and algorithms as well as talk about Java .
+* Here will be a new Interface implementation.
+*
+* This class will be used to get all the Elector data recorded with the
+* purpose of got it widely displayed in the screen.
+* @author  Hector Jose Vurchio Hurtado
+* @version 1.0
+* @since   2021-05-30 
+* @implements Comparable Interface with the sake of get a List sorted.
+* 
+*/
+
+class Elector2021Data implements Comparable<Elector2021Data>{
+	public String Id_Number;
+	public String Name;
+	public String Last_Name;
+	public LocalDate Birth_Date;
+	public String Gender;
+	public String Citizenship;
+	public String Party;
+	public String Status;
+	public LocalTime Time;
+	/**
+	* The constructor which build an object where enclose all 
+	* the fields required to print an elector data
+	*/		
+	Elector2021Data(Elector elector){
+		this.loadFields(elector);			
+	}
+	/**
+	* This method load the values to all fields of this class.
+	* @param elector, which brings all the elector information.
+	* @return nothing.
+	*/	
+	private void loadFields(Elector elector){
+		this.Id_Number = elector.getId();
+		this.Name = elector.getName();
+		this.Last_Name = elector.getLastName();	
+		this.Birth_Date = elector.getBirth();
+		this.Gender = elector.getGender();						
+		this.Citizenship = elector.getCitizenship();					
+		this.Party = elector.getParty();						
+		this.Status = elector.getVoteStatus();						
+		this.Time = elector.getTime();			
+	}
+	
+	/** 
+	*	Method from Comparable interface. String class implements 
+	*	Comparable interface as well.
+	*	@param Elector2021Data object.
+	*	@returns an int indicating the difference of both values.
+	*/
+	public int compareTo(Elector2021Data elect){
+		return this.Id_Number.compareTo(elect.Id_Number);
+	}
+}
